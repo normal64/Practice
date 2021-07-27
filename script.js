f3230c3a612de8cf381399025a574cf44140b923
Denis <normal64@mail.ru>
2021-07-27 21:58:54 +0400

    enter

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ function toggleMobileMenu(event) {
     document.querySelector("#menu").classList.toggle("active");
     document.querySelector(".mobile-bar").classList.toggle('active');
 }
+$(function (){
+    var star = '.star',
+        selected = '.selected';
+    
+    $(star).on('click', function(){
+      $(selected).each(function(){
+        $(this).removeClass('selected');
+      });
+      $(this).addClass('selected');
+    });
+   
+  });
